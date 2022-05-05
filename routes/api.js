bd7906c7725ceccd0e45b2c8829383845db2f2e5
Faraz Siddiqui <mohdfaraz.1.javarndcorp@gmail.com>
2022-05-05 14:10:02 -0400

    Added Autoscaling

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+
+const hostname = fs.readFileSync("/etc/hostname", "utf8").trim();
 
 // Login and Register routes
 router.get("/health", function (_req, res) {
   return res.json({
     health: "ok",
+    container: hostname,
   });
 });
 
